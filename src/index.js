4d56c25bfc6613139a4bdb14376489412678c5de
GauravSonar217 <ggsonar217@gmail.com>
2025-01-12 23:21:44 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
-require("dotenv").config(); 
+require("dotenv").config({ path: "./env" });
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/connection.js");
 
-connectDB();
 
-const PORT = process.env.PORT || 8000;
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
 
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log("Server is running on Port: 8000");
+        })
+
+    })
+    .catch((err) => {
+        console.log("MongoDB Connection failed !!! ", err);
+    })
